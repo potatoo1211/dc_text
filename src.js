782cb91fb500680233bec2188e32d7000d245455
Team-kawatama <gotturi4103@gmail.com>
2025-02-02 14:07:09 +0900

    Update src.js

--- src.js
+++ src.js
@@ -11,20 +11,20 @@ function resize(){
     if (16*H > 9*W) {
         $('.kawa').css({'position': 'relative', 'margin-top' : String((H-(9/16)*W)/2) + 'px', 'margin-left' : 0})
         $('.tama').css({'position': 'relative', 'width' : String(W) + 'px', 'height' : String((9/16)*W)})
-        for (i of texts){
+        for (var i of texts){
             if (i[2]){
                 var temp = document.getElementById(i[0]).innerText.length
-                $(i[0]).css({'position': 'absolute','width' : (String(W)*i[i]/(100*temp)) + 'px'})
-            } else $(i[0]).css({'position': 'absolute','width' : (String(W)*i[i]/100) + 'px'})
+                $(i[0]).css({'position': 'absolute','width' : String(W*i[1]/(100*temp)) + 'px'})
+            } else $(i[0]).css({'position': 'absolute','width' : String(W*i[1]/100) + 'px'})
         }
     } else {
         $('.kawa').css({'position': 'relative', 'margin-top' : 0, 'margin-left' : String((W-(16/9)*H)/2) + 'px'})
         $('.tama').css({'position': 'relative', 'width' : String((16/9)*H) + 'px', 'height' : String(H)})
-        for (i of texts){
+        for (var i of texts){
             if (i[2]){
                 var temp = document.getElementById(i[0]).innerText.length
-                $(i[0]).css({'position': 'absolute','width' : (String((16/9)*H)*i[i]/(100*temp)) + 'px'})
-            } else $(i[0]).css({'position': 'absolute','width' : (String((16/9)*H)*i[i]/100) + 'px'})
+                $(i[0]).css({'position': 'absolute','width' : String((16/9)*H*i[1]/(100*temp)) + 'px'})
+            } else $(i[0]).css({'position': 'absolute','width' : String((16/9)*H*i[1]/100) + 'px'})
         }
     }
 }
